2f04e1d7b7ab95539816b80d94e469071e7c23e2
VikasSingh1234 <75747727+VikasSingh1234@users.noreply.github.com>
2022-08-06 11:18:03 +0530

    Time: 6 ms (14.00%), Space: 6.3 MB (50.94%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    bool match(char a,char b){
+        if((a=='{' && b=='}') || (a=='[' && b==']')||(a=='(' && b==')')){
+            return true;
+        }
+        return false;
+    }
+    bool isValid(string s) {
+        stack<char> st;
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{'|| s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()==true)
+                    return false;
+                else if(match(st.top(),s[i])==false){
+                    return false;
+                }
+                else
+                {
+                    st.pop();
+                }
+            }
+        }
+        
+        return (st.empty()==true);
+    }
+};
