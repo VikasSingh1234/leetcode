a0e0a68e04972cd7ef93a63805ea4a193426603d
VikasSingh1234 <75747727+VikasSingh1234@users.noreply.github.com>
2022-10-27 16:35:16 +0530

    Time: 134 ms (59.58%), Space: 21.7 MB (42.13%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -10,22 +10,21 @@ public:
         int delrow[8] = {1,-1,0,0,1,-1,-1,1};
         int delcol[8] = {0,0,1,-1,1,-1,1,-1};
         
-        set<pair<int,pair<int,int>>> d;
+        queue<pair<int,pair<int,int>>> q;
         vector<vector<int>> dist(n,vector<int>(n,1e9));
         
-        d.insert({1,{0,0}});
+        q.push({1,{0,0}});
         dist[0][0] = 1;
-        while(!d.empty()){
-            auto it  = *(d.begin());
-            int steps = it.first;
-            int r = it.second.first;
-            int c = it.second.second;
+        while(!q.empty()){
+            int steps = q.front().first;
+            int r = q.front().second.first;
+            int c = q.front().second.second;
             
             if(r==n-1 &&  c==n-1){
                 return steps;
             }
             
-            d.erase(it);
+            q.pop();
             
             for(int i=0;i<8;i++){
                 int newrow = r+delrow[i];
@@ -34,19 +33,12 @@ public:
                 if(newrow>=0 && newrow<n && newcol>=0 && newcol<n && grid[newrow][newcol]==0){
                     if((steps+1)<dist[newrow][newcol]){
                         dist[newrow][newcol] = steps+1;
-                        d.insert({dist[newrow][newcol],{newrow,newcol}});
+                        q.push({dist[newrow][newcol],{newrow,newcol}});
                     }
                 }
             }
         }
         
-        
-        if(dist[n-1][n-1]!=1e9){
-            return dist[n-1][n-1];
-        }
-        else{
-            return -1;
-        }
-        
+        return -1;
     }
 };
