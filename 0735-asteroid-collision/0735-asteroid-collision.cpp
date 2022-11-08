1de45aad9e9a2965e6ffe14a83a719c163e5faa3
VikasSingh1234 <75747727+VikasSingh1234@users.noreply.github.com>
2022-11-08 12:49:32 +0530

    Time: 24 ms (70.36%), Space: 17.3 MB (99.05%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,58 +1,15 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> st;
         vector<int> ans;
-        
-        st.push(asteroids[0]);
-        int i = 1;
-        int n = asteroids.size();
-        
-        while(i<n){
-            if(!st.empty()){
-                if(st.top()>0){
-                    if(asteroids[i]<0){
-                        int replace =asteroids[i]; 
-                        int top;
-                        while(true && !st.empty()){
-                            top = st.top();
-                            if(top<0 || replace>0)
-                                break;
-                            st.pop();
-                            if(top!=(-replace)){
-                                if(top>(-replace)){
-                                    st.push(top);
-                                    replace = top;
-                                }    
-                            }
-                            else{
-                                break;
-                            }
-                        }
-                        if(top<-replace){
-                            st.push(replace);
-                        } 
-                    }
-                    else{
-                        st.push(asteroids[i]);
-                    }
-                }
-                else{
-                    st.push(asteroids[i]);
-                }
+        for (int i = 0; i < asteroids.size(); i++) {
+            if (asteroids[i] > 0 || ans.empty() || ans.back() < 0)
+                ans.push_back(asteroids[i]);
+            else if (ans.back() <= -asteroids[i]) {
+                if(ans.back() < -asteroids[i]) i--;
+                ans.pop_back(); 
             }
-            else{
-                st.push(asteroids[i]);
-            }
-            i++;
         }
-        
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
-        }
-        
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
