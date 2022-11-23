ed413abb07f6cfac358405e23ebdcdddb617aba4
VikasSingh1234 <75747727+VikasSingh1234@users.noreply.github.com>
2022-11-23 13:47:54 +0530

    Time: 16 ms (15.60%), Space: 9.8 MB (22.90%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,10 +4,12 @@ public:
         int low = 0;
         int high = nums.size()-1;
         int mid=0;
+        
         if(nums[0]>target)
             return 0;
         if(nums[high]<target)
             return high+1;
+        
         while(low<high){
             mid = low+(high-low)/2;
             if(nums[mid]==target)
