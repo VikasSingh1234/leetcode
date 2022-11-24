6db0b13cd49212cbd886b7e437816f62ec472eb1
VikasSingh1234 <75747727+VikasSingh1234@users.noreply.github.com>
2022-11-25 00:42:19 +0530

    Time: 664 ms (54.90%), Space: 8.1 MB (17.64%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -2,39 +2,34 @@ class Solution {
     int row[4]={1,-1,0,0};
     int col[4]={0,0,1,-1};
 public:
-    bool dfs(vector<vector<char>>& board, string word,int index,vector<vector<int>> &visited,int x,int y){
+    bool dfs(vector<vector<char>>& board, string word,int index,int x,int y){
         if(index==word.length()-1){
             return true;
         }
-        
-        visited[x][y] = 1;        
+        char ch=board[x][y];
+        board[x][y]='1';        
         for(int i = 0;i<4;i++){
             int newrow = x+row[i];
             int newcol = y+col[i];
             
-            if(newrow>=0 && newcol>=0 && newrow<board.size() && newcol<board[0].size() && !visited[newrow][newcol] && board[newrow][newcol]==word[index+1]){
-                if(dfs(board,word,index+1,visited,newrow,newcol)){
+            if(newrow>=0 && newcol>=0 && newrow<board.size() && newcol<board[0].size()&& board[newrow][newcol]==word[index+1]){
+                if(dfs(board,word,index+1,newrow,newcol)){
                     return true;
                 }
             }
         }
-        visited[x][y]=0;
+        board[x][y]=ch;
         return false;            
     }
     bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<int>> visited(board.size(),vector<int>(board[0].size(),0));
+       
         vector<vector<int>> temp(board.size(),vector<int>(board[0].size(),0));
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
                 if(board[i][j]==word[0]){
-                    if(dfs(board,word,0,visited,i,j)){
+                    if(dfs(board,word,0,i,j)){
                         return true;
-                    }
-                    else
-                    {
-                        visited=temp;
-                    }
-                  
+                    }                  
                 }
             }
         }
