b192ffae403d41fed3849afc162ae4b6441b7226
VikasSingh1234 <75747727+VikasSingh1234@users.noreply.github.com>
2022-11-28 16:36:54 +0530

    Time: 952 ms (80.76%), Space: 169.5 MB (65.38%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        vector<vector<int>> res;
-        vector<int> zero,one;
-        unordered_map<int,int> mpp;
+        map<int,int> win;
+        map<int,int> loss;
 
-        for(int i=0;i<matches.size();i++){
-            if(mpp.find(matches[i][0])==mpp.end()){
-                mpp[matches[i][0]] = 0;
-            }
+        int n = matches.size();
+        
+        for(int i=0;i<n;i++){
+            if(loss.find(matches[i][0])==loss.end())
+                win[matches[i][0]]++;
             
-            if(mpp.find(matches[i][1])!=mpp.end()){
-                mpp[matches[i][1]] += 1;  
-            }
-            else{
-                mpp[matches[i][1]] = 1;
-            }
-        }
-        // for(auto x: mpp){
-        //     cout<<x.first<<" "<<x.second<<endl;
-        // }
-        for(auto x:mpp){
-            if(x.second==0){
-                zero.push_back(x.first);
-            }
-            else if(x.second==1){
-                one.push_back(x.first);
+            loss[matches[i][1]]++;
+            
+            if(win.find(matches[i][1])!=win.end()){
+                win.erase(matches[i][1]);
             }
         }
         
-        sort(zero.begin(),zero.end());
-        sort(one.begin(),one.end());
-        res.push_back(zero);
-        res.push_back(one);
-        return res;
+        vector<int> w;
+        vector<int> l;
+        
+        for(auto x:win){
+            w.push_back(x.first);
+        }
+        
+        for(auto y:loss){
+            if(y.second==1)
+                l.push_back(y.first);
+        }
+        
+        return {w,l};
     }
 };
