64f43159cc6b8d33f7ff858eb2fe25174593ed32
VikasSingh1234 <75747727+VikasSingh1234@users.noreply.github.com>
2023-05-14 12:11:37 +0530

    Time: 243 ms (40.83%), Space: 8.8 MB (55.02%) - LeetHub

---
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxScore(vector<int>& nums) {
+        int maxStates = 1 << nums.size();
+        int finalMask = maxStates - 1;
+
+        vector<int> dp(maxStates);
+
+        for (int state = finalMask; state >= 0; state -= 1) {
+            if (state == finalMask) {
+                dp[state] = 0;
+                continue;
+            }
+
+            int numbersTaken = __builtin_popcount(state);
+            int pairsFormed = numbersTaken / 2;
+            
+            if (numbersTaken % 2) {
+                continue;
+            }
+
+            for (int firstIndex = 0; firstIndex < nums.size(); firstIndex += 1) {
+                for (int secondIndex = firstIndex + 1; secondIndex < nums.size(); secondIndex += 1) {
+                    if (((state >> firstIndex) & 1) == 1 || ((state >> secondIndex) & 1) == 1) {
+                        continue;
+                    }
+                    int currentScore = (pairsFormed + 1) * __gcd(nums[firstIndex], nums[secondIndex]);
+                    int stateAfterPickingCurrPair = state | (1 << firstIndex) | (1 << secondIndex);
+                    int remainingScore = dp[stateAfterPickingCurrPair];
+                    dp[state] = max(dp[state], currentScore + remainingScore);
+                }
+            }
+        }
+        return dp[0];
+    }
+};
