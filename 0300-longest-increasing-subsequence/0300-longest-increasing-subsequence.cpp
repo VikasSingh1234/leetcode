541ef037648505797739911065bae0f468479477
VikasSingh1234 <75747727+VikasSingh1234@users.noreply.github.com>
2023-06-16 10:24:24 +0530

    Time: 976 ms (22.75%), Space: 292.2 MB (14.48%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -17,7 +17,27 @@ public:
     }
     
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,-1));
-        return f(nums,0,-1,dp);
+        // vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,-1));
+        // return f(nums,0,-1,dp);
+        
+        
+        // tabulation
+        int n = nums.size();
+        
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prev_ind=ind-1;prev_ind>=-1;prev_ind--){
+                int len = dp[ind+1][prev_ind+1];
+        
+                if(prev_ind==-1 || nums[prev_ind]<nums[ind]){
+                    len = max(len,1+dp[ind+1][ind+1]);
+                }
+                
+                dp[ind][prev_ind+1] = len;
+            }
+        }
+        
+        return dp[0][0];
     }
 };
