3db30ac3e207946902d842059d832f37212674bf
VikasSingh1234 <75747727+VikasSingh1234@users.noreply.github.com>
2024-06-12 14:42:32 +0530

    Time: 61 ms (48.42%), Space: 31.5 MB (20.46%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        map<int,int> mpp;
+        sort(hand.begin(),hand.end());
+        
+        for(int i=0;i<hand.size();i++){
+            mpp[hand[i]]++;
+        }
+        
+        if(hand.size()%groupSize!=0){
+            return false;
+        }
+        
+        for(int i=0;i<hand.size();i++){
+            if(mpp[hand[i]]==0){
+                continue;
+            }
+            else{
+                int search = hand[i];
+                for(int j=0;j<groupSize;j++){
+                    if(mpp.find(search)!=mpp.end()){
+                        if(mpp[search]!=0){
+                            mpp[search]--;
+                            search++;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+        
+    }
+};
